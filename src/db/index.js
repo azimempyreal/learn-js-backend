05096b54354e5ad27c4df9e796613de8e05247f8
azimempyreal <azimh.empyreal@gmail.com>
2025-04-19 15:12:28 +0530

    connect mongoo db database.

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_Name } from "../constent.js";
+
+const connectDB = async () => {
+  try {
+    const conectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_Name}`
+    );
+    console.log(
+      `\n MongoDB Connected !! DB Host : ${conectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MongooDB Connection Error :- ", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
